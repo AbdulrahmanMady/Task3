b40641ef65a1253a7c3f434f5f65b6bac70c72a3
AbdoMady2002 <abdomady197@gmail.com>
2023-04-26 17:43:17 +0200

    Remove Dubegshow

--- task3/lib/Responsive/mobile.dart
+++ task3/lib/Responsive/mobile.dart
@@ -14,6 +14,7 @@ class _MobileScerrenState extends State<MobileScerren> {
       appBar: AppBar(
         title: const Text("mobile screen"),
       ),
+      bottomNavigationBar: BottomNavigationBar(items: []),
     );
   }
 }
