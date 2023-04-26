7d1417c40e384447678fd2d5115c44d1279442b7
AbdoMady2002 <abdomady197@gmail.com>
2023-04-26 20:59:19 +0200

    add bookmark_outline

--- task3/lib/Screens/home.dart
+++ task3/lib/Screens/home.dart
@@ -88,6 +88,10 @@ class _HomeState extends State<Home> {
                             Icons.send,
                           ),
                         ),
+                         IconButton(
+                          onPressed: () {},
+                          icon: Icon(Icons.bookmark_outline),
+                        ),
                       ],
                     ),
                   ])),
