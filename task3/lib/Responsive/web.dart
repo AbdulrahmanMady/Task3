b8676c38d6d16e2fb3bd171eb69a386aa1707ae0
AbdoMady2002 <abdomady197@gmail.com>
2023-04-26 13:02:37 +0200

    create responsice .dart

---
+++ task3/lib/Responsive/web.dart
@@ -0,0 +1,14 @@
+// ignore_for_file: implementation_imports
+
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+// ignore: camel_case_types
+class web extends StatelessWidget {
+  const web({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
