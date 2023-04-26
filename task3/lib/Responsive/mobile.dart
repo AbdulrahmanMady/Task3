45fc307f1d339f8641c188d04fa3470070d4ce94
AbdoMady2002 <abdomady197@gmail.com>
2023-04-26 19:07:16 +0200

    create items in mobile

--- task3/lib/Responsive/mobile.dart
+++ task3/lib/Responsive/mobile.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
 import 'color.dart';
+
 
 class MobileScerren extends StatefulWidget {
   const MobileScerren({Key? key}) : super(key: key);
@@ -11,18 +11,65 @@ class MobileScerren extends StatefulWidget {
 }
 
 class _MobileScerrenState extends State<MobileScerren> {
+  final PageController _pageController = PageController();
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("mobile screen"),
+        title: Text("AddPost Screen"),
       ),
       bottomNavigationBar: CupertinoTabBar(
-        backgroundColor: mobileBackgroundColor,
-      items: const [
-        BottomNavigationBarItem(icon: Icon(Icons.home), label: 'home'),
-        BottomNavigationBarItem(icon: Icon(Icons.search), label: 'home'),
-      ]),
+          backgroundColor: mobileBackgroundColor,
+          onTap: (index) {
+            _pageController.jumpToPage(index);
+          },
+          items: const [
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.home,
+                  color: secondaryColor,
+                ),
+                label: ""),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.search,
+                  color: primaryColor,
+                ),
+                label: ""),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.add_circle,
+                  color: secondaryColor,
+                ),
+                label: ""),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.favorite,
+                  color: secondaryColor,
+                ),
+                label: ""),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.person,
+                  color: secondaryColor,
+                ),
+                label: ""),
+          ]),
+      body: PageView(
+        onPageChanged: (index) {
+          print("------- $index");
+        },
+        //physics: NeverScrollableScrollPhysics(),
+        //controller: _pageController,
+       
+      ),
     );
   }
 }
