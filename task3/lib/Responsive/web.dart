3c021d2840adb0c6203bee0ef0b88d7c980b32e4
AbdoMady2002 <abdomady197@gmail.com>
2023-04-26 23:22:36 +0200

    nav Screen

--- task3/lib/Responsive/web.dart
+++ task3/lib/Responsive/web.dart
@@ -3,8 +3,9 @@ import '../Screens/home.dart';
 import '../screens/search.dart';
 import '../screens/addpost.dart';
 import '../screens/profile.dart';
-import 'color.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
+import 'color.dart';
 
 class WebScerren extends StatefulWidget {
   const WebScerren({Key? key}) : super(key: key);
@@ -14,80 +15,81 @@ class WebScerren extends StatefulWidget {
 }
 
 class _WebScerrenState extends State<WebScerren> {
-    final PageController _pageController = PageController();
-    
- navigate2Screen(int indexx) {
+  final PageController _pageController = PageController();
+
+  navigateScreen(int indexx) {
     _pageController.jumpToPage(indexx);
-    setState(() {
-      page = indexx;
-    });
+    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        actions: [
-          IconButton(
-            icon: Icon(
-              Icons.home,
-              color: primaryColor,
+          actions: [
+            IconButton(
+              icon: Icon(
+                Icons.home,
+                color: secondaryColor,
+              ),
+              onPressed: () {
+                navigateScreen(0);
+              },
             ),
-            onPressed: () {},
-          ),
-          IconButton(
-            icon: Icon(
-              Icons.search,
-              color: secondaryColor,
+            IconButton(
+              icon: Icon(
+                Icons.search,
+                color: secondaryColor,
+              ),
+              onPressed: () {
+                navigateScreen(1);
+              },
             ),
-            onPressed: () {},
-            _pageController.jumpToPage(1);
-          ),
-          IconButton(
-            icon: Icon(
-              Icons.add_a_photo,
-              color: secondaryColor,
+            IconButton(
+              icon: Icon(
+                Icons.add_a_photo,
+                color: secondaryColor,
+              ),
+              onPressed: () {
+                navigateScreen(2);
+              },
             ),
-            onPressed: () {},
-          ),
-          IconButton(
-            icon: Icon(
-              Icons.favorite,
-              color: secondaryColor,
+            IconButton(
+              icon: Icon(
+                Icons.favorite,
+                color: secondaryColor,
+              ),
+              onPressed: () {
+                navigateScreen(3);
+              },
             ),
-            onPressed: () {},
-          ),
-          IconButton(
-            icon: Icon(
-              Icons.person,
-              color: secondaryColor,
+            IconButton(
+              icon: Icon(
+                Icons.person,
+                color: secondaryColor,
+              ),
+              onPressed: () {
+                navigateScreen(4);
+              },
             ),
-            onPressed: () {},
-          ),
-           backgroundColor: mobileBackgroundColor,
-        title: SvgPicture.asset(
-          "ziadapp\asstes\img\download.jpg",
-          color: primaryColor,
-          height: 32,
-        ),
-      ),
-        body: PageView(
-           controller: _pageController,
-        onPageChanged: (index) {
-         
-        },
-        physics: NeverScrollableScrollPhysics(),
-        children: [
-          Home(),
-          Search(),
-
-                     Profile(),
-
-
-
-         
-        ],
-      ),
+          ],
+          backgroundColor: mobileBackgroundColor,
+          title: SvgPicture.asset(
+            "../asstes/img/ownload.jpg",
+            color: primaryColor,
+            height: 32,
+          )),
+    );
+    body:
+    PageView(
+      controller: _pageController,
+      onPageChanged: (index) {},
+      physics: NeverScrollableScrollPhysics(),
+      children: [
+        Home(),
+     
+        Profile(),
+      ],
     );
   }
 }
