44d3f629aa0789f57695ad6f8d1cdca50d738fdc
AbdoMady2002 <abdomady197@gmail.com>
2023-04-27 17:32:36 +0200

    add user margin

--- task3/lib/Screens/Profile.dart
+++ task3/lib/Screens/Profile.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import '../Responsive/color.dart';
 
 class Profile extends StatefulWidget {
@@ -13,97 +12,102 @@ class _ProfileState extends State<Profile> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-     appBar: AppBar(
-          backgroundColor: mobileBackgroundColor,
-          title: Text("ziadzakria"),
-        ),
-        body: Column(
+      appBar: AppBar(
+        backgroundColor: mobileBackgroundColor,
+        title: Text("abdo"),
+      ),
+      body: Column(children: [
+        Row(
           children: [
-            Row(
+            Container(
+              margin: EdgeInsets.only(left: 10),
+              padding: EdgeInsets.all(3),
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Color.fromARGB(125, 78, 91, 110),
+              ),
+              child: CircleAvatar(
+                radius: 26,
+                backgroundImage: NetworkImage(""
+                    ),
+              ),
+            ),
+            Column(
               children: [
-                Container(
-                    margin: EdgeInsets.only(left: 10),
-                  padding: EdgeInsets.all(3),
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: Color.fromARGB(125, 78, 91, 110),
+                Text(
+                  "1",
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
                   ),
-                  child: CircleAvatar(
-                    radius: 26,
-                    backgroundImage: NetworkImage(""),
-                  ),
-                ),
-                Column(
-                 children: [
-                        Text(
-                          "1",
-                          style: TextStyle(
-                            fontSize: 22,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        SizedBox(
-                          height: 5,
-                        ),
-                        Text(
-                          "Posts",
-                          style: TextStyle(
-                              fontSize: 15,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.grey),
-                        ),
-                      ],
-                    ),
-                    SizedBox(
-                  width: 17,
-                ),
-                Column(
-                  children: [
-                    Text(
-                      "8",
-                      style: TextStyle(
-                        fontSize: 22,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    SizedBox(
-                      height: 5,
-                    ),
-                    Text(
-                      "Followers",
-                      style: TextStyle(
-                          fontSize: 15,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.grey),
-                    ),
-                  ],
                 ),
                 SizedBox(
-                      height: 5,
-                    ),
-                    Column(
-                      children: [
-                        Text(
-                          "1000",
-                          style: TextStyle(
-                            fontSize: 22,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        SizedBox(
-                          height: 5,
-                        ),
-                        Text(
-                          "Following",
-                          style: TextStyle(
-                              fontSize: 15,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.grey),
-                        
-            )
+                  height: 5,
+                ),
+                Text(
+                  "Posts",
+                  style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.grey),
+                ),
+              ],
+            ),
+            SizedBox(
+              width: 17,
+            ),
+            Column(
+              children: [
+                Text(
+                  "8",
+                  style: TextStyle(
+                    fontSize: 10000,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                Text(
+                  "Followers",
+                  style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.grey),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 5,
+            ),
+            Column(
+              children: [
+                Text(
+                  "1000",
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                Text(
+                  "Following",
+                  style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.grey),
+                ),
+              ],
+            ),
           ],
         ),
-          ]);
+        Container(
+            margin: EdgeInsets.fromLTRB(15, 21, 0, 0),
+            width: double.infinity,
+            child: Text(" Cute & nice girl")),
+      ]),
+    );
   }
 }
-
