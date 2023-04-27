0ba5aeba4717b98487529f4b5c86ab14f1c72402
AbdoMady2002 <abdomady197@gmail.com>
2023-04-27 17:24:24 +0200

    add size box

--- task3/lib/Screens/Profile.dart
+++ task3/lib/Screens/Profile.dart
@@ -54,6 +54,9 @@ class _ProfileState extends State<Profile> {
                         ),
                       ],
                     ),
+                    SizedBox(
+                  width: 17,
+                ),
 
               )
               ],
