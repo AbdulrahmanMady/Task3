18a4606af59b2f54ce93cb898bd391f875295141
AbdoMady2002 <abdomady197@gmail.com>
2023-04-27 17:20:25 +0200

    add img avatar

--- task3/lib/Screens/Profile.dart
+++ task3/lib/Screens/Profile.dart
@@ -13,10 +13,28 @@ class _ProfileState extends State<Profile> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-         backgroundColor: mobileBackgroundColor,
-        title: Text("abdo"),
-      ),
-    );
+     appBar: AppBar(
+          backgroundColor: mobileBackgroundColor,
+          title: Text("ziadzakria"),
+        ),
+        body: Column(
+          children: [
+            Row(
+              children: [
+                Container(
+                  padding: EdgeInsets.all(3),
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Color.fromARGB(125, 78, 91, 110),
+                  ),
+                  child: CircleAvatar(
+                    radius: 26,
+                    backgroundImage: NetworkImage(""),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ));
   }
 }
